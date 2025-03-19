840dac08f2d3732d99479560b44ed51f3cf41f16
Apexshya <gyawaliapeksha123@gmail.com>
2025-03-19 19:42:37 +0545

    made changes on video

--- myproject/src/Components/Video.jsx
+++ myproject/src/Components/Video.jsx
@@ -5,7 +5,7 @@ const Video = () => {
   return (
     <Container fluid className="py-5">
       <Row className="justify-content-center">
-        <Col md={8} className="position-relative">
+        <Col md={12} className="position-relative">
           <Badge 
             bg="danger" 
             className="position-absolute top-0 start-0 m-4 fs-6"
@@ -14,13 +14,13 @@ const Video = () => {
             Youtube Video
           </Badge>
 
-          {/* Embedded YouTube Video */}
-          <div className="ratio ratio-16x9">
+          <div className="ratio ratio-21x9">
             <iframe 
               src="https://www.youtube.com/embed/dpEPNioJ1%7Ck?autoplay=1" 
               title="YouTube video"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
+              
             />
           </div>
         </Col>
